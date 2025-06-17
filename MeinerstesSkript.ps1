8437050233c14a28ce9c05e15eebc1ae96082b5e
Gottcollo <test@test.com>
2025-06-17 16:14:27 +0200

    Chore: Skript1

---
+++ MeinerstesSkript.ps1
@@ -0,0 +1,13 @@
+# Mein erstes PowerShell-Skript
+
+# Variablen definieren
+$name = "Samu"
+$alter = 32
+
+# Begrüßung und Vorstellung zusammensetzen
+$begrüßung = "Hallo, mein Name ist $name."
+$vorstellung = "Ich bin $name und ich bin $alter Jahre alt."
+
+# Ausgabe
+Write-Host $begrüßung
+Write-Host $vorstellung
